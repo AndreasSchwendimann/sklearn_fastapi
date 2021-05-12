bef7fcbcb605c3ee2497e069f06f20add0ac2563
piotrpawlaczek <info@pawlaczek.pl>
2021-05-12 16:33:56 +0200

    use query params

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from pydantic import  BaseModel
+from fastapi import FastAPI, Query
+from typing import List
+
+from pydantic import BaseModel
 from joblib import load
 import pandas as pd
 
@@ -20,24 +22,29 @@ class ModelParams(BaseModel):
     param13: float
     param14: float
 
+
 app = FastAPI()
 
-loaded_model = load('../model/classifier.joblib')
-loaded_scaler = load('../model/scaler.joblib')
-reference_table = pd.read_csv('../model/cutoff_table.csv')
+loaded_model = load("../model/classifier.joblib")
+loaded_scaler = load("../model/scaler.joblib")
+reference_table = pd.read_csv("../model/cutoff_table.csv")
 
 
 def get_prediction(data, duration):
-    print('start prediction ----------------------')
+    print("start prediction ----------------------")
     return apply_model(data, duration)
 
 
-def apply_model(data, duration, model=loaded_model, scaler=loaded_scaler, reference=reference_table):
+def apply_model(
+    data, duration, model=loaded_model, scaler=loaded_scaler, reference=reference_table
+):
     # apply scaler & model to new meeting
     data_scaled = scaler.transform(data)
     predicted_cluster = model.predict(data_scaled)[0]
     # subset reference & rank new meeting duration
-    if int(duration) > int(reference_table[reference_table.cluster == int(predicted_cluster)].cutoff):
+    if int(duration) > int(
+        reference_table[reference_table.cluster == int(predicted_cluster)].cutoff
+    ):
         show_alert = True
     else:
         show_alert = False
@@ -49,9 +56,14 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/predict/{param1}/{param2}/{param3}/{param4}/{param5}/{param6}/{param7}/{param8}/{param9}/{param10}/{param11}/{param12}/{param13}/{param14}")
-def predict(param1,param2,param3,param4,param5,param6,param7,param8,param9,param10,param11,param12,param13,param14):
-    data = [[param1,param2,param3,param4,param5,param6,param7,param8,param9,param10,param11,param12,param13,param14]]
-    pred = get_prediction(data, param8)
+@app.get("/predict/")
+def predict(
+    p8: float, params: List[str] = Query(["p1", "p2", "p3", "p4", "p5", "p6", "p7"])
+):
 
+    data = [list(map(float, params[0].split(",")))]
+    kwargs = {"param8": p8, "data": data}
+    print(kwargs)
+
+    pred = get_prediction(data, p8)
     return pred
